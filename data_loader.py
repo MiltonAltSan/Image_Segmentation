a9d61c5e908b42ee324ae7fc42a98d62057a2a8f
MiltonAltSan <66339220+MiltonAltSan@users.noreply.github.com>
2021-07-12 04:35:35 -0600

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -87,7 +87,8 @@ class ImageFolder(data.Dataset):
 		GT = Transform(GT)
 
 		#Norm_ = T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-		#image = Norm_(image)
+		Norm_ = T.Normalize((0.2, 0.2, 0.2), (0.5, 0.5, 0.5))
+		image = Norm_(image)
 
 		return image, GT
 
